dc6e2ca7b691cc8a6e6d268c7c4fdfff843defba
HannaOdud <annodud@gmail.com>
2026-04-29 17:28:51 +0100

    third commit

--- report.sql
+++ report.sql
@@ -53,3 +53,53 @@ JOIN courses ON courses.course_id = grades.course_id
 GROUP BY courses.course_id
 ORDER BY course_students DESC
 LIMIT 3;
+
+.print "9.List all students with their names and emails. LIMIT 5 "
+SELECT name, email
+FROM students
+LIMIT 5 ;
+
+.print "10.Show each student’s name, course name, and their grade. "
+SELECT students.name, grades.grade, courses.course_name
+FROM grades 
+JOIN courses ON courses.course_id = grades.course_id 
+JOIN students ON students.student_id = grades.student_id;
+
+
+.print "11. Find all students who scored above 90 in any course. "
+SELECT students.name, grades.grade, courses.course_name
+FROM grades 
+JOIN courses ON courses.course_id = grades.course_id 
+JOIN students ON students.student_id = grades.student_id
+WHERE grades.grade > 90;
+
+.print "12. Calculate the average grade for each student.  "
+SELECT students.name, ROUND(AVG(grades.grade),2)AS avg_grade
+FROM students
+JOIN grades ON grades.student_id = students.student_id
+GROUP BY students.student_id;
+
+.print "13. How many students are enrolled in each course?"
+SELECT COUNT(grades.student_id)AS stud_num, courses.course_name 
+FROM grades
+JOIN courses ON courses.course_id = grades.course_id
+GROUP BY courses.course_id;
+
+.print "14. Find the student(s) who got the highest grade in each course."
+SELECT students.name, MAX(grades.grade)AS max_grade_of_course, courses.course_name
+FROM grades
+JOIN students ON students.student_id = grades.student_id
+JOIN courses ON courses.course_id = grades.course_id
+GROUP BY courses.course_name;
+
+.print "15. List students who are enrolled in more than 4 courses."
+SELECT students.name, COUNT(grades.course_id)AS num_courses
+FROM students
+JOIN grades ON grades.student_id = students.student_id
+GROUP BY students.student_id
+HAVING num_courses > 4;
+
+.print "16. Find students whose average grade is above the overall average grade of all students."
+
+
+.print "17. Show the teacher name and the average grade of their course(s)."
